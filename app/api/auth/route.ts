f41fc397744ac6bcc5687a7c0c72a7a28f0364e0
Copicraft <vastagkoppany@gmail.com>
2026-06-01 20:07:30 +0200

    added cookies proppertly and tried doing top bar and its kinda wokring ig???

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,5 +1,6 @@
 import {NextResponse} from "next/server";
 import db from "../../lib/auth/db";
+import {cookies} from "next/headers";
 
 export async function POST(request: Request) {
     const body = await request.json();
@@ -8,6 +9,16 @@ export async function POST(request: Request) {
     const user = db.prepare("SELECT * FROM users WHERE username = ?").get(usr) as any; //ai recomended this part
 
     if (user && user.password === psw) {
+        const cookie = await cookies();
+
+        cookie.set("auth_session", user.username, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 60 * 60 * 24 * 7,
+            path: "/"
+        });
+
         return NextResponse.json({success:true, message:"YUPPEE!"})
     } else {
         return NextResponse.json({success:false, message:"not YUPPEE."})
